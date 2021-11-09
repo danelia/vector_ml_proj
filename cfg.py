bc820159bfc08d3d865155272964a0c580989175
danelia <gdane14@freeuni.edu.ge>
2021-11-09 19:46:04 +0400

    added config without my credentials

---
+++ cfg.py
@@ -0,0 +1,22 @@
+gcp_cfg = {
+    "project_id" : "",
+
+    "credentials" : {
+        "type": "",
+        "project_id": "",
+        "private_key_id": "",
+        "private_key": "",
+        "client_email": "",
+        "client_id": "",
+        "auth_uri": "",
+        "token_uri": "",
+        "auth_provider_x509_cert_url": "",
+        "client_x509_cert_url": ""
+    }
+}
+
+kafka_cfg = {
+    "kafka_servers": [
+            "localhost:9092"
+        ]
+}
